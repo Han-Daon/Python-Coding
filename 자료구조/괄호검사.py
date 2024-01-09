2a722ae372293ec1ad6a01318361c2f630797e0f
Han-Daon <lumin46@naver.com>
2024-01-09 13:12:41 +0900

    어디로 들어가냐

--- 자료구조/괄호검사.py
+++ 자료구조/괄호검사.py
@@ -1,16 +1,13 @@
 def checkBrackets(statement):
     stack = ArrayStack(100)
     for ch in statement:
-        if ch in ('{','[','('):
+        if ch in ('{' , '[' ,'('): #열리는 괄호 스택에 삽입
             stack.push(ch)
-        elif ch in ('}',']',')'):
+        elif ch in ('}',']',')'): # 닫히는 괄호시 스택 비어있으면...
             if stack.isEmpty() :
                 return False
-            else : 
+            else : #쌍이 맞지 않을때
                 left = stack.pop()
                 if (ch == "}" and left != "{") or (ch == "]" and left != "[") or (ch == ")" and left != "(") :
                        return False
     return stack.isEmpty()               
-                
-                
-                       
